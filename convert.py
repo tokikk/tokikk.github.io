d17172e24203033a9b30e15994cbc33c584b4157
toki <tokikake42@gmail.com>
2022-05-16 23:16:50 +0900

    add convert script

---
+++ convert.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+path_to_csv = './data/syukujitsu.csv'
+path_to_api = './api/' 
+
+if __name__ == '__main__':
+    csv_file = open(path_to_csv, 'r', encoding='utf-8', errors='', newline='')
+    obj = csv.reader(csv_file, delimiter=',', doublequote=True, lineterminator='\r\n', skipinitialspace=True)
+    next(obj)
+    os.makedirs('./api', exist_ok=True)
+    for row in obj:
+        nums = str(row[0]).split('/')
+        f = open(f'{path_to_api}{nums[0]}{nums[1].zfill(2)}{nums[2].zfill(2)}', 'w', encoding='UTF-8')
+        f.write(row[1])
+        f.close()
