f7126fb84e73e24f20b87d64b6078e5938531d37
tokikk <tokikake42@gmail.com>
2023-12-17 18:14:00 +0900

    変換処理を修正

--- convert.py
+++ convert.py
@@ -9,8 +9,11 @@ if __name__ == '__main__':
     obj = csv.reader(csv_file, delimiter=',', doublequote=True, lineterminator='\r\n', skipinitialspace=True)
     next(obj)
     os.makedirs('./api', exist_ok=True)
+    flist = open(f'{path_to_api}holidaylist', 'w', encoding='UTF-8')
     for row in obj:
         nums = str(row[0]).split('/')
+        flist.write(f'{nums[0]}{nums[1].zfill(2)}{nums[2].zfill(2)},{row[1]}\n')
         f = open(f'{path_to_api}{nums[0]}{nums[1].zfill(2)}{nums[2].zfill(2)}', 'w', encoding='UTF-8')
         f.write(row[1])
         f.close()
+    flist.close()
